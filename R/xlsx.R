419eaf0b3159c96c3d2f01c20c88f963700cee87
Toshikazu Matsumura <matutosi@gmail.com>
2023-04-16 06:36:48 +0900

    update vignette

--- R/xlsx.R
+++ R/xlsx.R
@@ -13,16 +13,9 @@
 #' 
 #' 
 #' 
-#' @export
-set_auto_filter <- function(sh){
-  xlsx::addAutoFilter(sh, "A1:Z1")
-}
-
-#' @export
-set_freeze_panel <- function(sh){
-  xlsx::createFreezePane(sh, 2, 2, 2, 2)
-}
-
+#' 
+#' 
+#' 
 #' @export
 set_af_fp <- function(file){
   wb <- xlsx::loadWorkbook(file)
@@ -31,4 +24,14 @@ set_af_fp <- function(file){
     set_freeze_panel(sh)
   }
   xlsx::saveWorkbook(wb, file)
+}
+
+#' @export
+set_auto_filter <- function(sh){
+  xlsx::addAutoFilter(sh, "A1:Z1")
+}
+
+#' @export
+set_freeze_panel <- function(sh){
+  xlsx::createFreezePane(sh, 2, 2, 2, 2)
 }
