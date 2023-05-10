913e89fec05c8812b6f4bd20081b4b01e63bdee2
Toshikazu Matsumura <matutosi@gmail.com>
2023-05-11 01:51:41 +0900

    add mouse_record()

--- R/keyboardsimulator.R
+++ R/keyboardsimulator.R
@@ -11,3 +11,24 @@ mouse_move_click <- function(x, y){
   KeyboardSimulator::mouse.click()
   sleep(0.5)
 }
+
+#' Wrapper function for recording mouse.get_cursor() position
+#' 
+#' @param n,interval A numeric of record number and interval (sec).
+#' @return  A list of x and y position.
+#' @examples
+#' mouse_record()
+#' 
+#' @export
+mouse_record <- function(n = 5, interval = 1){
+  x <- list()
+  y <- list()
+  for (i in seq(n)) {
+    automater::sleep(interval)
+      x[[i]] <- KeyboardSimulator::mouse.get_cursor()[1]
+      y[[i]] <- KeyboardSimulator::mouse.get_cursor()[2]
+      position <- paste0(i, ": x = ", x[[i]], ", y = ", y[[i]], "\n")
+      cat(position)
+  }
+  return(list(x = unlist(x), y = unlist(y)))
+}
