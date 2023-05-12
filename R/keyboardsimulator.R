15b97db308ef6c3fcc6c5b2123e1cd11e2c419d4
Toshikazu Matsumura <matutosi@gmail.com>
2023-05-12 10:50:39 +0900

    record step by step

--- R/keyboardsimulator.R
+++ R/keyboardsimulator.R
@@ -15,6 +15,7 @@ mouse_move_click <- function(x, y){
 #' Wrapper function for recording mouse.get_cursor() position
 #' 
 #' @param n,interval A numeric of record number and interval (sec).
+#'        "interval < 0" means wait user input (any keys on R console).
 #' @return  A list of x and y position.
 #' @examples
 #' mouse_record()
@@ -24,11 +25,15 @@ mouse_record <- function(n = 5, interval = 1){
   x <- list()
   y <- list()
   for (i in seq(n)) {
-    automater::sleep(interval)
-      x[[i]] <- KeyboardSimulator::mouse.get_cursor()[1]
-      y[[i]] <- KeyboardSimulator::mouse.get_cursor()[2]
-      position <- paste0(i, ": x = ", x[[i]], ", y = ", y[[i]], "\n")
-      cat(position)
+    if(interval < 0){
+      user_input("Press any keys on R console")
+    }else{
+      automater::sleep(interval)
+    }
+    x[[i]] <- KeyboardSimulator::mouse.get_cursor()[1]
+    y[[i]] <- KeyboardSimulator::mouse.get_cursor()[2]
+    position <- paste0(i, ": x = ", x[[i]], ", y = ", y[[i]], "\n")
+    cat(position)
   }
   return(list(x = unlist(x), y = unlist(y)))
 }
