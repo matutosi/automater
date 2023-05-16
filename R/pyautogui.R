b1b17b799e89c0d16133c5c19e7edde24ace5d69
Toshikazu Matsumura <matutosi@gmail.com>
2023-05-17 01:56:08 +0900

    for pyautogui

---
+++ R/pyautogui.R
@@ -0,0 +1,74 @@
+#' Wrapper function for PyAutoGUI to recognize and click image.
+#' 
+#' @param img  A string of path for image to recognize.
+#' @param pag  A python.builtin.module built 
+#'             by reticulate::import("pyautogui")
+#' @param wait A logical. 
+#'             TRUE: Wait untill finding a image.
+#'             FALSE: When not found image, return error.
+#' @param ...  More arguments to pass pag$locateOnScreen().
+#' @return     A linvisible list ($x, $y) of clicked position.
+#' @examples
+#' python_path <- find_python()
+#' reticulate::use_python(python_path)
+#' pag <- reticulate::import("pyautogui")
+#' img <- fs::path_package("automater", "img", "up_arrow.png")
+#' recog_image_click(igm, pag)
+#' 
+#' @export
+recog_image_click <- function(img, pag, wait = TRUE, ...){
+  position <- pag$locateOnScreen(img, ...)
+  if(wait){
+    while(is.null(position)){
+      sleep(0.5)
+      position <- pag$locateOnScreen(img, ...)
+    }
+  }
+  if(is.null(position)){
+    stop("img not foud: ", img)
+  }
+  pos <- center(position)
+  mouse_move_click(pos$x, pos$y)
+  return(invisible(pos))
+}
+
+#' Helper function for recog_image_click() to get center of position.
+#' 
+#' @param position  A numeric
+#' @return  A numeric list ($x, $y) of position.
+#' @export
+center <- function(position){
+  x <- position$left + (position$width / 2)
+  y <- position$top +  (position$height / 2)
+  return(list(x = x, y = y))
+}
+
+#' Helper function for recog_image_click().
+#' 
+#' @param corner  A string to specify a corner. 
+#'                "top_left", "top_right", "bottom_left", or "bottom_right".
+#' @param width,height  A integer.
+#' @export
+display_corner <- function(corner = "bottom_right", width = 600, height = 600){
+  size <- display_size()
+  corner <- 
+    switch(corner,
+      "top_left"     = c(                 0,                   0, width, height), 
+      "top_right"    = c(size$width - width,                   0, width, height), 
+      "bottom_left"  = c(                 0, size$height- height, width, height), 
+      "bottom_right" = c(size$width - width, size$height- height, width, height)
+    )
+  return(as.integer(corner))
+}
+
+#' Helper function for display_corner() to get display size.
+#' 
+#' @return     A numeric list ($width, $height) of display size.
+#' @export
+display_size <- function(){
+  pos <- KeyboardSimulator::mouse.get_cursor()
+  KeyboardSimulator::mouse.move(999999,999999)
+  size <- KeyboardSimulator::mouse.get_cursor()
+  KeyboardSimulator::mouse.move(pos[1], pos[2])
+  return(list(width = size[1], height = size[2]))
+}
